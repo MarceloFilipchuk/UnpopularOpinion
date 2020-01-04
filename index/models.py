fde77ce45bcf1731762bda8535d98731d2420705
= <=>
2020-01-03 21:52:25 -0300

    Se agregaron las apps al settings, y se crearon las bases de datos

--- index/models.py
+++ index/models.py
@@ -3,4 +3,9 @@ from datetime import date
 
 
 # Create your models here.
-
+class Post(models.Model):
+    content = models.TextField(max_length=512)
+    owner = models.CharField(max_length=20)
+    positive_points = models.BigIntegerField()
+    negative_points = models.BigIntegerField()
+    date = models.DateField(default=date.today())
